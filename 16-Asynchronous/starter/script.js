396bc8dd49ec4b1785e7f0bd6ccf4f6bdf4383ee
Diego Cerqueira <xdiegocerqueira@gmail.com>
2024-11-06 17:35:29 +0100

    coding challenge #3 - Promises and async image loading

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -2,6 +2,7 @@
 
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
+const imgContainer = document.querySelector('.images');
 
 ///////////////////////////////////////
 
@@ -315,8 +316,6 @@ const getPosition = function () {
 
 // btn.addEventListener('click', alert('asdasdsadas'));
 btn.addEventListener('click', locateMe);
-// getPosition().then(response => console.log(response));
-// });
 
 // TEST COORDINATES 1: 52.508, 13.381 (Latitude, Longitude)
 // TEST COORDINATES 2: 19.037, 72.873
@@ -324,3 +323,74 @@ btn.addEventListener('click', locateMe);
 // whereAmI('-33.933', '18.474');
 // whereAmI('-33.933', '18.474');
 // whereAmI('-33.933', '18.474');
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+Build the image loading functionality that I just showed you on the screen.
+
+Tasks are not super-descriptive this time, so that you can figure out some stuff on your own. Pretend you're working on your own 😉
+
+PART 1
+1. Create a function 'createImage' which receives imgPath as an input. This function returns a promise which creates a new image (use document.createElement('img')) and sets the .src attribute to the provided image path. When the image is done loading, append it to the DOM element with the 'images' class, and resolve the promise. The fulfilled value should be the image element itself. In case there is an error loading the image ('error' event), reject the promise.
+
+If this part is too tricky for you, just watch the first part of the solution.
+
+PART 2
+2. Comsume the promise using .then and also add an error handler;
+3. After the image has loaded, pause execution for 2 seconds using the wait function we created earlier;
+4. After the 2 seconds have passed, hide the current image (set display to 'none'), and load a second image (HINT: Use the image element returned by the createImage promise to hide the current image. You will need a global variable for that 😉);
+5. After the second image has loaded, pause execution for 2 seconds again;
+6. After the 2 seconds have passed, hide the current image.
+
+TEST DATA: Images in the img folder. Test the error handler by passing a wrong image path. Set the network speed to 'Fast 3G' in the dev tools Network tab, otherwise images load too fast.
+
+GOOD LUCK 😀
+*/
+
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+const loadImage = imgPath => {
+  return new Promise(function (resolve, reject) {
+    let img = document.createElement('img');
+    img.src = imgPath;
+
+    img.addEventListener('load', () => {
+      imgContainer.append(img);
+      resolve(img);
+    });
+
+    img.addEventListener('error', () => {
+      reject(new Error('Could not load image.'));
+    });
+  });
+};
+
+let currentImg = '';
+loadImage('img/img-1.jpg')
+  .then(img => {
+    currentImg = img;
+    console.log('loading image.');
+    return wait(2); // waits two seconds
+  })
+  .then(() => {
+    console.log('remove background');
+    currentImg.style.display = 'none';
+    return loadImage('img/img-2.jpg');
+  })
+  .then(img => {
+    currentImg = img;
+    console.log('after two seconds this is img promise');
+    return wait(2);
+  })
+  .then(() => {
+    console.log('loading image2.');
+    console.log(currentImg);
+    console.log('remove background');
+    currentImg.style.display = 'none';
+  });
